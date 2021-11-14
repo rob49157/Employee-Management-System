b786096fa8e14a392623aa510dbd40fbd01c635e
roberto <robertopaipo@hotmail.com>
2021-11-14 11:42:51 -0500

    index.js

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ const PORT = process.env.PORT || 3000;
 const router =express.Router()
 const fs = require("fs")
 
+employeeoptions()
+function employeeoptions(){
+    inquirer.prompt({
+        type:"list",
+        message:"what would you like to do?",
+        name:"role",
+        choices:["add department", "add role", 'add employee']
+
+    })
+.then(function(employeeoptions){
+    if (role === "add department") {
+        inquirer.prompt([
+            {type:'input',
+            message:"which department:",
+            name:'department'},
+
+            {type:"list",
+            message:"what would you like to do?",
+            name:"role",
+            choices:["add department", "add role", 'add employee']}
+        ])
+    }
+}
+            
+        
