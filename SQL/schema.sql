57a83096485ffb6fea67d4b3a81ac77e65834713
roberto <robertopaipo@hotmail.com>
2021-11-14 11:04:17 -0500

    schema table

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -9,15 +9,27 @@ job_title VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
 );
 
-CREATE TABLE employee_data(
-    employeedata VARCHAR(50) NOT NULL,
-    employee_ids INT NOT NULL,
+CREATE TABLE Department(
+    department_name VARCHAR(50) NOT NULL,
+    ID INT NOT NULL,
+    
+    FOREIGN KEY (roles),
+    REFERENCES roles(id);
+);
+CREATE TABLE roles(
+    id INT NOT NULL,
+    Title VARCHAR(50),
+    Salary INT NOT NULL,
+    FOREIGN KEY (roles),
+    REFERENCES roles(id);
+    
+CREATE TABLE employee(
+    
+    ID INT NOT NULL,
     first_names VARCHAR(30),
     last_names VARCHAR(30),
-    job_titles VARCHAR(30),
-    deparments INT,
-    salaries INT,
-    managers VARCHAR(30),
+    role_id INT NOT NULL,
+    managers_ID INT,
     FOREIGN KEY (roles),
     REFERENCES roles(id)
-);
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser")
-const fs = require("inquirer")
+const inquirer = require("inquirer")
 const app = express()
 const PORT = process.env.PORT || 3000;
 const router =express.Router()
+const fs = require("fs")
 
