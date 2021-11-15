03f71e789ede532043f39b00ba34ad9990867413
rob49157 <robertopaipo@hotmail.com>
2021-11-14 20:40:30 -0500

    seeds

---
+++ SQL/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO Department(id,department_name)
+VALUES()
+
+
+INSERT INTO roles(id,Title,Salary,deparments_id)
+VALUES()
+
+
+INSERT INTO employee(ID,first_names,last_names,role_id,managers_ID)
+VALUES()
