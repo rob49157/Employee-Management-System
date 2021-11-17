419d19d38e7c28e5ce731041591c57937d0adf83
rob49157 <robertopaipo@hotmail.com>
2021-11-17 13:59:59 -0500

    joining issues

--- db/query.sql
+++ db/query.sql
@@ -1,4 +1,5 @@
 SELECT *
 FROM role
-JOIN department on department.id = role.department_id;
+JOIN department on department.id = role.department_id
+JOIN employee on employee.role_id = department.id
 
